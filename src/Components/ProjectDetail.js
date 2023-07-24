5b1407ecdc6f917bbbaa8e602a9308369e760a71
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2023-07-24 18:38:21 +0700

    -Image: add image3 -ProjectDetail + Portfolio: edit content

--- src/Components/ProjectDetail.js
+++ src/Components/ProjectDetail.js
@@ -7,7 +7,7 @@ AOS.refresh();
 
 const project = ({ project }) => {
     console.log(project);
-    return(
+    return (
         <div className="row">
             <div className="col-12 ">
                 <h2 className="text-uppercase text-center portfolio-heading">mockup project</h2>
@@ -31,35 +31,92 @@ const project = ({ project }) => {
                     </li>
 
                     <li className="mb-2">
-                        <span className="portfolio-detail-title">{project.project_position_title}: </span>
-                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block">{project.project_position_value}</span>
-                    </li>
-
-                    <li className="mb-2">
-                        <span className="portfolio-detail-title">{project.project_work_title}: </span>
-                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block">{project.project_work_value}</span>
+                        <span className="portfolio-detail-title">{project.project_team_size_title}: </span>
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block">{project.project_team_size_value}</span>
                     </li>
 
                     <li className="mb-2">
                         <span className="portfolio-detail-title">{project.project_source_title}: </span>
                         <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block">{project.project_source_value}</span>
                     </li>
-
-                    <li className="mb-2">
-                        <span className="portfolio-detail-title">{project.project_description_title}: </span>
-                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block">{project.project_description_value}</span>
-                    </li>
-                </ul>                                            
+                </ul>
             </div>
 
             <div className="col-xl-12 col-lg-12 col-12">
-                <iframe width="100%" height="650" 
-                        src={project.project_video_link} 
-                        title="YouTube video player"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                        allowFullScreen>
+                <ul className="portfolio-detail-list list-unstyled">
+                    <li>
+                        <span className="portfolio-detail-title">Video Demo:</span>
+                    </li>
+                </ul>
+                <iframe width="100%" height="400"
+                    src={project.project_video_link}
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen>
                 </iframe>
             </div>
+
+
+            <div>
+                <span className="portfolio-detail-title">{project.project_tech_title}</span>
+                <ul className="portfolio-detail-list list-unstyled">
+                    <li className="mb-2">
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block ms-3">{project.project_tech_value1}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block ms-3">{project.project_tech_value2}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block ms-3">{project.project_tech_value3}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block ms-3">{project.project_tech_value4}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block ms-3">{project.project_tech_value5}</span>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <ul className="portfolio-detail-list list-unstyled">
+                    <li className="mt-2 mb-2">
+                        <span className="portfolio-detail-title">{project.project_description_title}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block">{project.project_description_value1}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block">{project.project_description_value2}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block">{project.project_description_value3}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block">{project.project_description_value4}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block">{project.project_description_value5}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block">{project.project_description_value6}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block">{project.project_description_value7}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block">{project.project_description_value8}</span>
+                    </li>
+                    <li className="mt-2">
+                        <span className="portfolio-detail-title">{project.project_work_title}: </span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block ms-3">{project.project_work_value1}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block ms-5 mt-2 mb-2">{project.project_work_value2}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block ms-5 mt-2 mb-2">{project.project_work_value3}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block ms-5 mt-2 mb-2">{project.project_work_value4}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block ms-3">{project.project_work_value5}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block ms-3">{project.project_work_value6}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block ms-3">{project.project_work_value7}</span>
+                        <br />
+                        <span className="portfolio-detail-value d-block d-sm-inline-block d-lg-block d-xl-inline-block ms-3">{project.project_work_value8}</span>
+                    </li>
+                </ul>
+            </div>
+
         </div>
     )
 }
